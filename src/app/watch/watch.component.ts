8f3aea88bce05d15e97724af71fc2fd080fd32b1
akshataborute27 <akshataborute@gmail.com>
2025-10-03 23:29:59 +0530

    add changes

--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-watch',
   templateUrl: './watch.component.html',
-  styleUrls: ['./watch.component.css']  // ✅ Make sure this file exists
+  styleUrls: ['./watch.component.css']
 })
-export class WatchComponent { }
+export class WatchComponent {
+  @ViewChild('slider', { static: true }) slider!: ElementRef;
+
+  scrollLeft() {
+    this.slider.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.slider.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+}
